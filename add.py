7b8292a3c7f544b4159dc672cb819454775dc12a
Teja Maheswar <2200080216.aids@gmail.com>
2025-02-07 09:54:05 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,4 @@
+num1 = 2
+num2 = 5
+sum = num1 + num2
+print('the sum of {0} and (1) is {2}[D[D[D[D[D[D[}[D[[C{1[D[[[C[[C[3~[3~[3~[3~[C[C[C[C[C[C {1} is {2}'.format(num1, num2, sum)
